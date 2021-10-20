c59a781960a049a1b5de339d4c20c9f5bc479c62
Administrator <admin@example.com>
2021-10-20 21:40:44 +0900

    edit for lab

--- lab02/outputs.tf
+++ lab02/outputs.tf
@@ -1,7 +1,3 @@
 output "catapp_url" {
   value = "http://${ncloud_public_ip.hashicat.public_ip}"
 }
-
-output "ssh_pw" {
-  value = nonsensitive("sshpass -p '${data.ncloud_root_password.hashicat.root_password}' ssh root@${ncloud_public_ip.hashicat.public_ip} -oStrictHostKeyChecking=no")
-}
