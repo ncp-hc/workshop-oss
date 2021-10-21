d4e2cc685327fbaf3592e01fdc9d4872b3187c03
Administrator <admin@example.com>
2021-10-21 11:14:05 +0900

    delete remote backend tf

--- lab02/remote_backend.tf
+++
@@ -1,9 +0,0 @@
-// terraform {
-//   backend "remote" {
-//     hostname = "app.terraform.io"
-//     organization = "YOURORGANIZATION"
-//     workspaces {
-//       name = "hashicat-ncp"
-//     }
-//   }
-// }
